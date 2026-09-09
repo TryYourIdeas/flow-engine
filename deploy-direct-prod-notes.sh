c6ecb83fb04bf5a25a19932fd745f107bf015f24
Juan Labrada <agile@juanlabrada.com>
2026-09-09 18:28:43 -0300

    config: production deploy

---
+++ deploy-direct-prod-notes.sh
@@ -0,0 +1,32 @@
+# get release notes
+PREV_TAG=$(git rev-parse production/current)
+
+# create release notes
+VERSION_TAG=$(node -p -e "require('./package.json').version")
+NOTES=$(git log $PREV_TAG..HEAD --oneline)
+
+echo "Release version $VERSION_TAG"
+echo "=================================" >> NEW_RELEASE_NOTES.txt
+echo "Release version $VERSION_TAG" >> NEW_RELEASE_NOTES.txt
+echo "------------------------------" >> NEW_RELEASE_NOTES.txt
+echo $NOTES >> NEW_RELEASE_NOTES.txt
+cat RELEASE_NOTES.txt >> NEW_RELEASE_NOTES.txt
+rm RELEASE_NOTES.txt
+mv NEW_RELEASE_NOTES.txt RELEASE_NOTES.txt
+
+# using -f to add node_modules
+git add .output -f
+git add RELEASE_NOTES.txt
+git commit -m "config: preparing release"
+
+# delete previous production current
+git tag -d production/current
+git push production --delete production/current
+
+# update  production current
+VERSION_TAG=$(node -p -e "require('./package.json').version")
+git tag -a production/current -m "Release version $VERSION_TAG"
+
+#push
+git push production production/current
+
---
+++ deploy-direct-prod.sh
@@ -0,0 +1,29 @@
+CURRENT_BRANCH=$(git branch --show-current)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo "You are not on the main branch. Please switch to the main branch and try again."
+  exit 1
+fi
+
+rm .output -rf
+git add .output
+git commit -m "config: remove old build"
+
+PREV_TAG=$(git rev-parse production/current)
+# fix, bugfix, secfix, refactor, docs, config changes: are patch changes.
+# feature: are minor changes
+# breaking: are major changes
+CHANGE=patch
+if git log $PREV_TAG..HEAD --grep="feature:" -i | grep -q .; then
+  CHANGE=minor
+fi
+if git log $PREV_TAG..HEAD --grep="feat:" -i | grep -q .; then
+  CHANGE=minor
+fi
+if git log $PREV_TAG..HEAD --grep="breaking:" -i | grep -q .; then
+  CHANGE=major 
+fi
+
+# set the new release version number
+npm version $CHANGE -m "Release version: %s"
+
+
